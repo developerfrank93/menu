ca8591f2fb2cf7314c5d1e1e67edd16089ff5b1e
developerfrank93 <87324942+developerfrank93@users.noreply.github.com>
2021-07-12 18:09:50 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+  //PARAMETRI DI CONFIGURAZIONE
+	$HOST = "127.0.0.1";
+	$DATABASE = "account";
+	$USERNAME = "root";
+	$PASSWORD = "";
+	$CHARSET = array(PDO::MYSQL_ATTR_INIT_COMMAND  => 'SET NAMES \'UTF8\'');
+
+?>
