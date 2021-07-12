9b1d029ddcd18439abb57d88cedc73b552ec62d3
developerfrank93 <87324942+developerfrank93@users.noreply.github.com>
2021-07-12 18:09:26 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,97 +1,31 @@
 <?php
 
-  if(isset($_GET['var'])){
+  echo '<!DOCTYPE html>
+        <html>
+          <head>
+            <title>Registration</title>
+            <link href="style.css" rel="stylesheet" type="text/css">
+          </head>
+          <body>';
 
-  }
-  else{
-    $_GET['var'] = "citta";
-    $_GET['var2'] = "manduria";
-  }
-
-  if(isset($_GET['var2'])){
-
-  }
-  else{
-    $_GET['var2'] = "manduria";
-  }
-  echo'
-  <!DOCTYPE html>
-    <html>
-      <head>
-        <title></title>
-        <link href="style.css" rel="stylesheet" type="text/css">
-      </head>
-    <body>
-      <div id="box">
-        <div id="menu">
-          <a class="link1" href="index.php?var=mare">mare</a>';
-
-
-          if($_GET['var'] == "mare"){
-            echo '
-              <a class="link2" href="index.php?var=mare&var2=blu">mare del brasile</a>
-                <a class="link2" href="index.php?var=mare&var2=blu">mare mediterraneo</a>
-                <a class="link2" href="index.php?var=mare&var2=blu">mare indiano</a>
-                <a class="link2" href="index.php?var=mare&var2=blu">mare americano</a>
-            ';
-          }
-
-
-          echo'<a class="link1" href="index.php?var=montagne">Montagne</a>';
-
-          if($_GET['var'] == "montagne"){
-            echo '
-              <a class="link2" href="index.php?var=montagne&var2=everest">everest</a>
-              <a class="link2" href="index.php?var=montagne&var2=monte bianco">monte bianco</a>
-              <a class="link2" href="index.php?var=montagne&var2=kilimangiaro">kilimangiaro</a>
-            ';
-          }
-
-          echo'<a class="link1" href="index.php?var=fiori">Fiori</a>';
-
-          if($_GET['var'] == "fiori"){
-            echo '
-              <a class="link2" href="index.php?var=fiori&var2=viola">viola</a>
-              <a class="link2" href="index.php?var=fiori&var2=girasole">girasole</a>
-              <a class="link2" href="index.php?var=fiori&var2=orchidea">orchidea</a>
-              <a class="link2" href="index.php?var=fiori&var2=rosa">rosa</a>
-              <a class="link2" href="index.php?var=fiori&var2=ciliegio">ciliegio</a>
-            ';
-          }
-
-          echo'<a class="link1" href="index.php?var=citta">Città</a>';
-
-          if($_GET['var'] == "citta"){
-            echo '
-              <a class="link2" href="index.php?var=citta&var2=manduria">Manduria</a>
-              <a class="link2" href="index.php?var=citta&var2=bari">Bari</a>
-              <a class="link2" href="index.php?var=citta&var2=lecce">lecce</a>
-              <a class="link2" href="index.php?var=citta&var2=gallipoli">gallipoli</a>
-              <a class="link2" href="index.php?var=citta&var2=otranto">otranto</a>
-              <a class="link2" href="index.php?var=citta&var2=ostuni">ostuni</a>
-              <a class="link2" href="index.php?var=citta&var2=napoli">napoli</a>
-              <a class="link2" href="index.php?var=citta$var2=napoli">pescara</a>
-            ';
-          }
-
-          echo'<a class="link1" href="index.php?var=hitech">HiTech</a>';
-
-          if($_GET['var'] == "hitech"){
-            echo '
-              <a class="link2" href="index.php?var=hitech&var2=smartphone">smartphone</a>
-              <a class="link2" href="index.php?var=hitech&var2=pc">PC</a>
-              <a class="link2" href="index.php?var=hitech&var2=monitor">monitor</a>
-            ';
-          }
-
-        echo'</div>
-        <div id="center">';
-
-        echo'<img src="' . $_GET['var2'] . '.jpg">';
-
-        echo'</div>
-      </div>
-    </body>
-    </html>
-  ';
-?>
+  echo '<form action="registrazione.php" method="POST">
+          <div id="boxlog">
+            <div id="bxlg0">
+              <div id="bxlg1">Registration</div>
+				    </div>
+            <div id="boxlogcenter">
+              <div class="boxinp">USERNAME:</div>
+				      <input class="inp" name="username" type="text" />
+              <div class="boxinp">PASSWORD: </div>
+              <input class="inp" name="password" type="text" />
+              <div class="boxinp">EMAIL: </div>
+              <input class="inp" name="email" type="text" />
+              <div id="boxlogbottom">
+              <input id="submit" type="submit" value="REGIST" />
+              <input id="reset" type="reset" value="RESET" />
+            </div>
+        </div>
+        </div>
+		   </form>
+       </body>
+       </html>';
